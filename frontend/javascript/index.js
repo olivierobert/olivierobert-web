2e762d77f6b35dda325b5d9ce6baa532454dfb6d
Olivier Robert <olivierobert@gmail.com>
2023-09-10 09:17:33 +0200

    Customize and configure the site (#1)
    * Customize the README
    * Clanup config file
    * Structure the stylesheet directory 💅
    * Add GitHub-related repository templates
    * Customize generated content

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,5 +1,4 @@
-import "$styles/index.scss"
-import "$styles/syntax-highlighting.css"
+import "$stylesheets/application.scss"
 
 // Import all JavaScript & CSS files from src/_components
 import components from "$components/**/*.{js,jsx,js.rb,css}"
